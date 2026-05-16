ae29112a6fdc8d9bb2a19d0a7a2e6426ead76c1f
telalim1988 <ta.elalim@gmail.com>
2026-05-17 02:02:04 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
-// Main Application File
+// SELECT BUTTON
+
+const calculateBtn = document.getElementById("calculateBtn");
+
+
+// BUTTON EVENT
+
+calculateBtn.addEventListener("click", function () {
+
+    // READ INPUT VALUES
+
+    const rainfall =
+        document.getElementById("rainfall").value;
+
+    const area =
+        document.getElementById("area").value;
+
+    const runoff =
+        document.getElementById("runoff").value;
+
+    const slope =
+        document.getElementById("slope").value;
+
+    const manning =
+        document.getElementById("manning").value;
+
+
+    // PRINT VALUES TO CONSOLE
+
+    console.log("Rainfall:", rainfall);
+
+    console.log("Area:", area);
+
+    console.log("Runoff:", runoff);
+
+    console.log("Slope:", slope);
+
+    console.log("Manning:", manning);
+
+});
