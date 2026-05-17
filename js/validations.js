72985778419b8ff59503dfd842651223d170d596
telalim1988 <ta.elalim@gmail.com>
2026-05-17 19:27:53 +0400

    Update validations.js

--- js/validations.js
+++ js/validations.js
@@ -1,1 +1,57 @@
-// Input Validations
+// ============================================
+// INPUT VALIDATION
+// ============================================
+
+function validateInputs(
+    rainfall,
+    area,
+    runoff,
+    slope,
+    manning
+) {
+
+    // CHECK EMPTY VALUES
+
+    if (
+        rainfall === "" ||
+        area === "" ||
+        runoff === "" ||
+        slope === "" ||
+        manning === ""
+    ) {
+
+        return {
+            valid: false,
+            message:
+                "Please fill in all input fields."
+        };
+
+    }
+
+
+    // CHECK NEGATIVE VALUES
+
+    if (
+        rainfall <= 0 ||
+        area <= 0 ||
+        runoff <= 0 ||
+        slope <= 0 ||
+        manning <= 0
+    ) {
+
+        return {
+            valid: false,
+            message:
+                "All values must be greater than zero."
+        };
+
+    }
+
+
+    // VALID INPUTS
+
+    return {
+        valid: true
+    };
+
+}
